5fd9adcb31312ca3471549fda21ef63494f1d128
Matthew Cawley <mjcwly@gmail.com>
2020-11-10 09:10:13 +0000

    Added example of creating a custom ErrorHandler class that logs to loupe with any uncaught exceptions.

--- src/Loupe.Angular.Demo/ClientApp/src/app/app.module.ts
+++ src/Loupe.Angular.Demo/ClientApp/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
@@ -9,6 +9,7 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { MyErrorHandler } from './error-handler/error-handler';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,9 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
       { path: 'fetch-data', component: FetchDataComponent },
     ])
   ],
-  providers: [],
+  providers: [
+    { provide: ErrorHandler, useClass: MyErrorHandler }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
