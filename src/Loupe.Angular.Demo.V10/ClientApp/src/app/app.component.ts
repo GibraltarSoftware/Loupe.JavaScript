d83c15a6279093312a308859a5a1dacdbaa77ea5
Matthew Cawley <mjcwly@gmail.com>
2020-11-11 12:55:54 +0000

    Now setting the session ID in AppComponent

--- src/Loupe.Angular.Demo.V10/ClientApp/src/app/app.component.ts
+++ src/Loupe.Angular.Demo.V10/ClientApp/src/app/app.component.ts
@@ -14,6 +14,9 @@ export class AppComponent {
     private router: Router,
     private loupe: LoupeService
   ) {
+    loupe.setSessionId('cb8f7be8-89b9-46cb-96b7-c406fe34c952');
+    loupe.setCORSOrigin('https://localhost:44325/');
+
     this.router.events.pipe(
       filter(x => x instanceof NavigationStart)
     ).subscribe((evnt: RouterEvent) => {
