931237432d2d7a8eecdf75dd5cd06dc47084aab2
Matthew Cawley <mjcwly@gmail.com>
2020-11-12 10:11:55 +0000

    Now providing the LoupeErrorHandler from within the LoupeAngularModule

--- src/loupe-angular/projects/loupe-angular/src/lib/loupe-angular.module.ts
+++ src/loupe-angular/projects/loupe-angular/src/lib/loupe-angular.module.ts
@@ -1,10 +1,14 @@
+import { ErrorHandler } from '@angular/core';
 import { NgModule } from '@angular/core';
 import { LoupeAngularComponent } from './loupe-angular.component';
+import { LoupeErrorHandler } from './loupe-error-handler';
 
 @NgModule({
   declarations: [LoupeAngularComponent],
-  imports: [
-  ],
-  exports: [LoupeAngularComponent]
+  imports: [],
+  exports: [LoupeAngularComponent],
+  providers: [
+    { provide: ErrorHandler, useClass: LoupeErrorHandler }
+  ]
 })
 export class LoupeAngularModule { }
