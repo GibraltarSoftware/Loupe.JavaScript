7c6202a2ef8cbaeef9546f4c46b37047527b71bf
Dave Sussman <davids@ipona.com>
2020-11-19 11:21:33 +0000

    Changed default CORS port to the one used by the Loupe.Agent.Core demo site

--- src/Loupe.Angular.Demo/ClientApp/src/app/app.component.ts
+++ src/Loupe.Angular.Demo/ClientApp/src/app/app.component.ts
@@ -15,14 +15,14 @@ export class AppComponent {
     private loupe: LoupeService
   ) {
     loupe.setSessionId('6745bc1e-e719-4bfe-b1ee-8bea50f2b17b');
-    loupe.setCORSOrigin('https://localhost:44360/');
+    loupe.setCORSOrigin('https://localhost:44348/');
 
     this.router.events.pipe(
       filter(x => x instanceof NavigationStart)
     ).subscribe((evnt: RouterEvent) => {
       this.loupe.information(
-        "Angular", "NavigationStart", evnt.url,
-        null, null, JSON.stringify(evnt), null
+        'Angular', 'NavigationStart', evnt.url,
+        null, null, evnt, null
       );
     });
   }
