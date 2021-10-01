4fea7ac6993e80424b29fd074d555693c27e6ece
Dave Sussman <davids@ipona.com>
2021-10-01 13:54:06 +0100

    Updated public API. Added Loupe HTTP Interceptor to Angular demos. Added loupe-typescript to Angular demos to allow for resolution of MethodSourceInfo via the npm import. Updated version numbers.

--- src/loupe-typescript/src/index.ts
+++ src/loupe-typescript/src/index.ts
@@ -1,7 +1,7 @@
 export { Exception } from './Exception';
 export { Header } from './Header';
 export { ILocalPlatform } from './LocalPlatform';
-import { LocalStorageMessage } from './LocalStorageMessage';
-import { LogMessageSeverity } from './LogMessageSeverity';
+export { LocalStorageMessage } from './LocalStorageMessage';
+export { LogMessageSeverity } from './LogMessageSeverity';
 export { LoupeAgent } from './Loupe.Agent';
 export { MethodSourceInfo } from './MethodSourceInfo';
